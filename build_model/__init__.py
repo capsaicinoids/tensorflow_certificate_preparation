7dd676418952a77609a3d98692b3da46ff596b6b
capsaicinoids <frizaaditya09@gmail.com>
2023-06-29 21:03:48 +0700

    Add Submission A2

--- build_model/__init__.py
+++ build_model/__init__.py
@@ -1,10 +1,9 @@
 import urllib.request
 import zipfile
-# from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import pandas as pd
 import numpy as np
-
 
 def solution_A1():
     # DO NOT CHANGE THIS CODE
@@ -14,3 +13,51 @@ def solution_A1():
                  12.0, 13.0, 14.0, ], dtype=float)
     
     return X, Y
+
+def solution_A2():
+    # data_url_1 = 'https://github.com/dicodingacademy/assets/releases/download/release-horse-or-human/horse-or-human.zip'
+    # urllib.request.urlretrieve(data_url_1, 'horse-or-human.zip')
+    # local_file = 'horse-or-human.zip'
+    # zip_ref = zipfile.ZipFile(local_file, 'r')
+    # zip_ref.extractall('data/train-horse-or-human')
+
+    # data_url_2 = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/validation-horse-or-human.zip'
+    # urllib.request.urlretrieve(data_url_2, 'validation-horse-or-human.zip')
+    # local_file = 'validation-horse-or-human.zip'
+    # zip_ref = zipfile.ZipFile(local_file, 'r')
+    # zip_ref.extractall('data/validation-horse-or-human')
+    # zip_ref.close()
+
+    TRAINING_DIR = 'data/train-horse-or-human'
+    VALIDATION_DIR = 'data/validation-horse-or-human'
+    
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        horizontal_flip=True,
+        shear_range=0.2,
+        zoom_range=0.2,
+        fill_mode='nearest'
+    )
+
+    validation_datagen = ImageDataGenerator(
+        rescale=1/255
+    )
+
+    # YOUR IMAGE SIZE SHOULD BE 150x150
+    train_generator = train_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+
+    validation_generator = validation_datagen.flow_from_directory(
+        VALIDATION_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+
+    return train_generator, validation_generator
+
--- build_model/submission_a.py
+++ build_model/submission_a.py
@@ -1,18 +1,24 @@
-from build_model import solution_A1
+from build_model import solution_A1, solution_A2
 from typing import List, Tuple
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, LSTM, Bidirectional
 
+class AccuracyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') > 0.83 and logs.get('val_accuracy') > 0.83):
+            print("\Both Accuracy is more than 83%, stopping...")
+            self.model.stop_training = True
+
 class Hyperparameters:
     def __init__(self):
-        self.neurons: Tuple[float] = None
-        self.epochs: int = None
-        self.batch_size: int = None
-        self.activation: str = None
-        self.optimizer: str = None
-        self.loss: str = None
-        self.metrics: List[str] = None
+        self._neurons: Tuple[float] = None
+        self._epochs: int = None
+        self._batch_size: int = None
+        self._activation: Tuple[str] = None
+        self._optimizer: str = None
+        self._loss: str = None
+        self._metrics: List[str] = None
 
     @property
     def neurons(self) -> Tuple[float]:
@@ -39,11 +45,11 @@ class Hyperparameters:
         self._batch_size = value
 
     @property
-    def activation(self) -> str:
+    def activation(self) -> Tuple[str]:
         return self._activation
 
     @activation.setter
-    def activation(self, value: str):
+    def activation(self, value: Tuple[str]):
         self._activation = value
 
     @property
@@ -71,13 +77,27 @@ class Hyperparameters:
         self._metrics = value
 
 class SubmissionA1(Hyperparameters):
-
-    def __init__(self):
+    def __init__(self, input_shape: Tuple[int]):
         super().__init__()
+        self.input_shape: Tuple[int] = input_shape
+    
+    @property
+    def input_shape(self) -> Tuple[int]:
+        return self._input_shape
+
+    @input_shape.setter
+    def input_shape(self, value: Tuple[int]):
+        self._input_shape = value
 
     def build_layers(self):
-        layers = [Dense(units=neuron, activation=self.activation, input_shape = [1]) for neuron in self.neurons]
-        model = tf.keras.Sequential(layers)
+        # layers = [Dense(units=neuron, activation=act, input_shape=self.input_shape if i == 0 else None) for i, (neuron, act) in enumerate(zip(self.neurons, self.activation))]
+        raw_layers = []
+        for i, (neuron, act) in enumerate(zip(self.neurons, self.activation)):
+            if i == 0:
+                raw_layers.append(Dense(units=neuron, activation=act, input_shape=self.input_shape))
+            else:
+                raw_layers.append(Dense(units=neuron, activation=act))
+        model = tf.keras.Sequential(raw_layers)
 
         return model
     
@@ -91,29 +111,47 @@ class SubmissionA1(Hyperparameters):
         model = self.build_model()
         X, Y = solution_A1()
         
-        model.fit(X, Y, epochs=self.epochs,)
+        model.fit(X, Y, epochs=self.epochs)
 
         return model
     
-class SubmissionA2(SubmissionA1):
-    def __init__(self, filters: Tuple[int], kernel_size: Tuple[int], activation_conv2d: str):
+class SubmissionA2(Hyperparameters):
+    def __init__(self, input_shape: Tuple[int], filters: Tuple[int], kernel_size: Tuple[int], activation_conv2d: str):
         super().__init__()
-        filters: Tuple[int] = filters
-        kernel_size: Tuple[int] = kernel_size
-        activation_conv2d: str = activation_conv2d
+        self.input_shape: Tuple[int] = input_shape
+        self.filters: Tuple[int] = filters
+        self.kernel_size: Tuple[int] = kernel_size
+        self.activation_conv2d: str = activation_conv2d
 
     def build_layers(self):
-        dense_layers = super().build_layers()
-        cnn_layers = []
+        raw_cnn_layers = []
 
-        for filter in self.filters: 
-            cnn_layers.append(Conv2D(filters=filter, kernel_size=self.kernel_size, activation=self.activation_conv2d))
-            cnn_layers.append(MaxPool2D(pool_size=(2, 2)))
+        for index, filter in enumerate(self.filters): 
+            if index == 0:
+                raw_cnn_layers.append(Conv2D(filters=filter, kernel_size=self.kernel_size, activation=self.activation_conv2d, input_shape=self.input_shape))
+            else:
+                raw_cnn_layers.append(Conv2D(filters=filter, kernel_size=self.kernel_size, activation=self.activation_conv2d))
+            raw_cnn_layers.append(MaxPool2D(pool_size=(2, 2)))
+
+        raw_cnn_layers.append(Flatten())
+        raw_cnn_layers.append(Dense(self.neurons[0], activation=self.activation[0]))
+        raw_cnn_layers.append(Dense(self.neurons[1], activation=self.activation[1]))
         
-        model = tf.keras.Sequential(cnn_layers + dense_layers)
+        model = tf.keras.Sequential(raw_cnn_layers)
 
         return model
     
     def build_model(self):
-        return super().build_model()
+        model = self.build_layers()
+        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
+        
+        return model
     
+    def train_model(self):
+        model = self.build_model()
+        callback = AccuracyCallback()
+        X, Y = solution_A2()
+        
+        model.fit(X, validation_data=Y, epochs=self.epochs, callbacks=[callback])
+
+        return model
